3665a49c3752792a11be8c4edf749a277cd64eef
fran-rs <56495962+fran-rs@users.noreply.github.com>
2026-08-22 17:33:18 +0000

    feat: create initial fullstack project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+   return (
+    <div>
+      <h1>Mi Recetario</h1>
+      <p>Bienvenidos a mi aplicación de recetas</p>
+    </div>
+  )
+}
+
+export default App
