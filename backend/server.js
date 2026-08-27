5fd9626fab9b924b7c52530ee6d78be8e50bbe22
fran-rs <56495962+fran-rs@users.noreply.github.com>
2026-08-27 16:59:39 +0000

    feat: connect React  with recipes Api

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 const PORT = 3000
 
 app.get('/', (req, res) => {
   res.send('Hola desde mi backend')
+})
+
+app.get('/api/recipes', (req, res) => {
+  const recipes = [
+    {
+      id: 1,
+      name: 'Pasta con tomate'
+    },
+    {
+      id: 2,
+      name: 'Torta de chocolate'
+    },
+    {
+      id: 3,
+      name: 'Ensalada Cesar'
+    }
+  ]
+
+  res.json(recipes)
 })
 
 app.listen(PORT, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,49 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+import { useEffect, useState } from 'react'
+
 function App() {
+  const [recipes, setRecipes] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  useEffect(() => {
+    fetch('http://localhost:3000/api/recipes')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Error al obtener las recetas')
+      }
+
+      return response.json()
+    })
+    .then((data) => {
+      setRecipes(data)
+      setLoading(false)
+    })
+    .catch(() => {
+      setError('No se pudieron guardar las recetas')
+      setLoading(false)
+    })
+  }, [] )
+
    return (
     <div>
       <h1>Mi Recetario</h1>
       <p>Bienvenidos a mi aplicación de recetas</p>
+
+      {loading && <p>Cargando recetas...</p>}
+
+      {error && <p>{error}</p>}
+
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            {recipe.name}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
